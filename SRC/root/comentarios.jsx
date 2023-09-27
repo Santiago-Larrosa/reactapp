66abbeca04d53d93618a3d127e9f8663b00cfb35
Santiago-Larrosa <130399338+Santiago-Larrosa@users.noreply.github.com>
2023-09-27 06:44:21 -0300

    Update comentarios.jsx

--- SRC/root/comentarios.jsx
+++ SRC/root/comentarios.jsx
@@ -1,18 +1,25 @@
+import React from "react";
 
 
-export default function Coment() {
-    return (
-      <>
-        <div>
+export default function Comentarios() {
+  const [comentarios, setComentarios] = useState([]);
+  useEffect(() => {
+    // Obtén los comentarios de localStorage
+    const comentariosJson = localStorage.getItem('comentarios');
+
+    // Asigna los comentarios al estado
+    setComentarios(comentariosJson ? JSON.parse(comentariosJson) : []);
+  }, []);
+  return (
+    <>
+      <div>
         <h2>Lista de Comentarios:</h2>
-        {/*} <ul>
-          {comentarios.map((comentario, i) => (
-            <li key={i}>
-              <strong>{comentario.nombre}:</strong> {comentario.mensaje}
-            </li>
+        <ul>
+          {comentarios.map((comentario, index) => (
+            <p key={index}>{comentario.nombre}: {comentario.mensaje}: {comentario.textoComentario}</p>
           ))}
-          </ul>*/}
+        </ul>
       </div>
-          </>
-    );
-  }
+    </>
+  );
+}
