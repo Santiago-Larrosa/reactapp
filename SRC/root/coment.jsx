011a85843dd3c52c9489585773e0b78166bc557a
Santiago-Larrosa <130399338+Santiago-Larrosa@users.noreply.github.com>
2023-09-28 17:53:20 -0300

    Update coment.jsx

--- SRC/root/coment.jsx
+++ SRC/root/coment.jsx
@@ -4,14 +4,16 @@ import { useState, useEffect } from "react";
 export default function Coment() {
   const [comentarios, setComentarios] = useState([]);
   const [id, setid] = useState(0);
+  const [go, setgo] = useState(0);
 
   useEffect(() => {
     const comentariosJson = localStorage.getItem("comentarios");
-    setComentarios(comentariosJson ? JSON.parse(comentariosJson) : []);
+  setComentarios(comentariosJson ? JSON.parse(comentariosJson) : []);
 
     // Obtén el id del comentario del parámetro
-    const id = window.location.href.split("?")[1].split("=")[1];
-    setid(id);
+    const queryParams = new URLSearchParams(window.location.search);
+  const idParam = queryParams.get("id");
+  setgo(idParam);
 
     // Si el objeto comentarios está vacío, muestra un mensaje de error
     if (comentarios.length === 0) {
@@ -25,10 +27,15 @@ export default function Coment() {
         <h2>Detalle del comentario</h2>
 
         {comentarios.filter((comentario) => {
-          return comentario.id === id;
-        }).map((comentario, index) => (
-          <p key={index}>{comentario.nombre}: {comentario.mensaje}: {comentario.textoComentario} :{comentario.id+index}</p>
-        ))}
+          return comentario.id === go;
+        }).map((comentario, index) => {
+          // Agrega un if para comprobar si el comentario coincide con go
+          if (comentario.id === go) {
+            return (
+              <p key={index}>{comentario.nombre}: {comentario.mensaje}: {comentario.textoComentario}: {comentario.id+index} </p>
+            );
+          }
+        })}
 
         <button>
                 <a href={`/`}>Inicio</a>
