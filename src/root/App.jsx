d869da5c9ddc5301fada5ea7efea75994e502a17
Santiago-Larrosa <130399338+Santiago-Larrosa@users.noreply.github.com>
2023-10-03 12:17:05 -0300

    Update App.jsx

--- src/root/App.jsx
+++ src/root/App.jsx
@@ -3,6 +3,7 @@ import './Appstyle.css';
 
 function TuComponente() {
   const [nombre, setNombre] = useState('');
+  const [Titulo, setTitulo] = useState('');
   const [mensaje, setMensaje] = useState('');
   const [comentarios, setComentarios] = useState([]);
   const fechaInicial = new Date();
@@ -22,6 +23,7 @@ function TuComponente() {
     const nuevoComentario = {
       nombre,
       mensaje,
+      Titulo,
       id: nuevoId,
       hora:(nuevahoras),
       minutos:(nuevaminuto),
@@ -32,6 +34,7 @@ function TuComponente() {
     setComentarios([...comentarios, nuevoComentario]);
     setNombre('');
     setMensaje('');
+    setTitulo('');
   };
 
   const saveComentarios = () => {
@@ -52,10 +55,15 @@ function TuComponente() {
   }, []);
 
   return (
+    <>
+     <header className="Head">
+    <a href="/app"><img src=".\SRC\root\13007.png" alt="mensaje" className="imagen"  /></a> 
+    <a href="/"><img src=".\SRC\root\13009.png" alt="mensaje" className="imagen2" /></a>
+      
+    </header>
     <div>
-      <h1 className="titulo"> <i>CREAR POST </i></h1>
-      <h1 className="titulo1"> <i>CREAR POST </i></h1>
-      <h1 className="titulo2"> <i>CREAR POST </i></h1>
+      
+ 
       <form onSubmit={handleSubmit}>
         <div>
           <label className="Nombre" htmlFor="nombre"></label>
@@ -66,6 +74,17 @@ function TuComponente() {
             placeholder='Nombre'
             value={nombre}
             onChange={(e) => setNombre(e.target.value)}
+          />
+        </div>
+        <div>
+          <label className="Titulor" htmlFor="Titulo"></label>
+          <input
+          className='InTitulo'
+            type="text"
+            id="titulo"
+            placeholder='Titulo'
+            value={Titulo}
+            onChange={(e) => setTitulo(e.target.value)}
           />
         </div>
         <div>
@@ -81,10 +100,10 @@ function TuComponente() {
         <button className='botonazo'>Crear Post</button>
      
       </form>
-      <button className='botonazo2'><a  href="/"> Inicio</a></button>
+      
     </div>
+    </>
   );
 }
 
 export default TuComponente;
-
