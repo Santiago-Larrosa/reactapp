d3776ee23a39df9c64bdf2864a4b73b133ba21d7
Santiago-Larrosa <130399338+Santiago-Larrosa@users.noreply.github.com>
2023-10-06 21:46:53 -0300

    Update coment.jsx

--- src/root/coment.jsx
+++ src/root/coment.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./comentStyle.css";
-import Markdown from 'react-markdown';
-import remarkGfm from 'remark-gfm'; 
+//import Markdown from 'react-markdown';
+//import remarkGfm from 'remark-gfm'; 
 import imagen from"./13009.png";
 import imagen2 from"./13007.png";
 
@@ -88,7 +88,7 @@ export default function Coment() {
             <div className="PostComent">
               <p>
                 <b className="negro"> {comentarioSeleccionado.nombre}</b><br />
-                <Markdown remarkPlugins={[remarkGfm]}>{comentarioSeleccionado.mensaje}</Markdown> <br />
+                { /*<Markdown remarkPlugins={[remarkGfm]}>{comentarioSeleccionado.mensaje}</Markdown>*/} <br />
               </p>
             </div>
 
