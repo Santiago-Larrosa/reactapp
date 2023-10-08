4552a30648158bcb8a25080827a235e0c7366d95
Santiago-Larrosa <130399338+Santiago-Larrosa@users.noreply.github.com>
2023-10-08 00:05:56 -0300

    Update root.jsx

--- src/root/root.jsx
+++ src/root/root.jsx
@@ -1,36 +1,47 @@
 import React, { useState, useEffect } from "react";
 import './rootstyle.css';
-import imagen from"./13009.png";
-import imagen2 from"./13007.png";
+
 export default function Root() {
   const [comentarios, setComentarios] = useState([]);
-  const [go, setGo] = useState(null); 
+  const [go, setGo] = useState(null);
   const [admin, setAdmin] = useState(false);
   const [showButton, setShowButton] = useState(false);
-  const [borrar, setBorrar]= useState('');
-  useEffect (()=> {
+  
+ 
+const [hora, setHora] = useState(new Date()); // Inicializa con la hora actual
+  const [borrar, setBorrar] = useState('');
+
+  // Función para actualizar la hora cada segundo
+  function tick() {
+    const nuevaFecha = new Date();
+    setHora(nuevaFecha);
+  }
+
+  useEffect(() => {
+    const timerId = setInterval(tick, 1000);
+
+    // Limpieza del intervalo al desmontar el componente
+    return () => clearInterval(timerId);
+  }, []);
+
+  useEffect(() => {
     const DownAdmin = localStorage.getItem("admin");
     setAdmin(JSON.parse(DownAdmin));
-    if (admin){
+
+    if (admin) {
+      setShowButton(admin);
+    } else {
       setShowButton(admin);
     }
-    else {
-      setShowButton(admin);
-    }
+
     setShowButton(JSON.parse(DownAdmin));
     console.log(showButton);
     console.log(admin);
-  })
-
- 
-
-
-  
- 
-  
+  }, [admin]);
 
   useEffect(() => {
     const comentariosJson = localStorage.getItem("comentarios");
+
     if (comentariosJson) {
       const comentariosArray = JSON.parse(comentariosJson);
       setComentarios(comentariosArray);
@@ -43,62 +54,58 @@ export default function Root() {
       setGo(idParam);
     }
   }, []);
-    
- const onButtom = () => {
-  const idBorrar = JSON.getItem(comentarios);
-  setBorrar(comentarios.id);
-  console.log(borrar);
 
- }
- const HandleClick = (id) => {
-  // Filtra el comentario principal y todos los comentarios vinculados
-  const updatedComentarios = comentarios.filter((comentario) => comentario.id !== id);
-  setComentarios(updatedComentarios);
-  localStorage.setItem("comentarios", JSON.stringify(updatedComentarios));
-};
+  const onButtom = () => {
+    const idBorrar = JSON.getItem(comentarios);
+    setBorrar(comentarios.id);
+    console.log(borrar);
+  }
+
+  const HandleClick = (id) => {
+    // Filtra el comentario principal y todos los comentarios vinculados
+    const updatedComentarios = comentarios.filter((comentario) => comentario.id !== id);
+    setComentarios(updatedComentarios);
+    localStorage.setItem("comentarios", JSON.stringify(updatedComentarios));
+  };
 
   return (
     <>
-    <header className="Head">
-    <a href="/app"><img src={imagen2} alt="mensaje" className="imagen"  /></a> 
-    <a href="/"><img src={imagen} alt="mensaje" className="imagen2" /></a>
-      
-    </header>
-    
+      <header className="Head">
+        <a href="/app"><img src=".\SRC\root\13007.png" alt="mensaje" className="imagen" /></a>
+        <a href="/"><img src=".\SRC\root\13009.png" alt="mensaje" className="imagen2" /></a>
+      </header>
+
       <div className="sidebar">
-      
         <div>
-         
-          <form method="post">
-            
-          </form>
+          <form method="post"></form>
         </div>
-        <nav>
-         
-              
-           
-          </nav>
-          
-          <div className="EsteDiv">
-        <ul>
-          {comentarios.map((comentario, index) => (
-            <div key={index} className="Post">
-              <h2 className="">{comentario.nombre}</h2>
-              <h1 className="TituloPost">{comentario.Titulo}</h1>
-              <p>{comentario.hora}:{comentario.minutos}:{comentario.segundos}</p>
-              <button className="Boton">
-                <a onClick={() => console.log(comentario.id)} href={`/coment/${parseInt(comentario.id)}`}>COMENTAR</a> </button>
+        <nav></nav>
+        <div className="Reloj">
+          <h1>
+            {hora.getHours().toString().padStart(2,0)}:
+            {hora.getMinutes().toString().padStart(2,0)}:
+            {hora.getSeconds().toString().padStart(2,0)}
+          </h1>
+        </div>
+        <div className="EsteDiv">
+          <ul>
+            {comentarios.map((comentario, index) => (
+              <div key={index} className="Post">
+                <h2 className="">{comentario.nombre}</h2>
+                <h1 className="TituloPost">{comentario.Titulo}</h1>
+                <p>{comentario.hora}:{comentario.minutos}:{comentario.segundos}</p>
+                <button className="Boton">
+                  <a onClick={() => console.log(comentario.id)} href={`/coment/${parseInt(comentario.id)}`}>COMENTAR</a>
+                </button>
                 {showButton && (
-                  <button className="Borrar" onClick={() => HandleClick(comentario.id)} >BORRAR</button>
+                  <button className="Borrar" onClick={() => HandleClick(comentario.id)}>BORRAR</button>
                 )}
-             
-            </div>
-))}
-  
-        </ul>
-      </div>
+              </div>
+            ))}
+          </ul>
         </div>
-        <div id="detail"></div>
-      </>
+      </div>
+      <div id="detail"></div>
+    </>
   );
 }
