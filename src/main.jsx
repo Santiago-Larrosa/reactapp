a34933f8ad5e93045b2f4c241fed9f752437357d
Santiago-Larrosa <130399338+Santiago-Larrosa@users.noreply.github.com>
2023-10-09 16:14:06 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-//import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from 'react-markdown';
 import {
   createBrowserRouter,
   RouterProvider,
