131ae66bae4c34ce8dd6fd20db66616987b01d28
Santiago-Larrosa <130399338+Santiago-Larrosa@users.noreply.github.com>
2023-10-11 09:50:52 -0300

    Update App.jsx

--- src/root/App.jsx
+++ src/root/App.jsx
@@ -112,7 +112,7 @@ const [contador, setContador] = useState(() => {
           />
         </div>
         
-        <button className='botonazo' {/*onClick={() => navigate("/")}*/}>Crear <br></br>Post</button>
+        <button className='botonazo' >Crear <br></br>Post</button>
         
      
       </form>
